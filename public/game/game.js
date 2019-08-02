cfe3e14027d5c329d8bc1e7b569596060d665ba9
TBGS iMac <tbgsimac@TBGSui-iMac-2.local>
2019-08-03 01:26:46 +0900

    developing : mouse, touch control / node for max for handling user data

---
+++ public/game/game.js
@@ -0,0 +1,135 @@
+var config = {
+  type: Phaser.AUTO,
+  parent: 'mmorpm',
+  width: 1080,
+  height: 1920,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+      gravity: {
+        y: 0
+      }
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image('ship', 'assets/img/character.png')
+  this.load.image('otherPlayer', 'assets/img/character.png')
+}
+
+function create() {
+  var self = this;
+  this.socket = io()
+  this.otherPlayers = this.physics.add.group();
+  //'currentPlayers'이벤트 받으면  할일
+  this.socket.on('currentPlayers', function(players) {
+    Object.keys(players).forEach(function(id) {
+      if (players[id].playerId === self.socket.id) {
+        addPlayer(self, players[id])
+      } else {
+        addOtherPlayers(self, players[id])
+      }
+    })
+  });
+  //'newPlayer'받으면 할일
+  this.socket.on('newPlayer', function(playerInfo) {
+    addOtherPlayers(self, playerInfo);
+  });
+  //'disconnect'받으면 할일
+  this.socket.on('disconnect', function(playerId) {
+    self.otherPlayers.getChildren().forEach(function(otherPlayer) {
+      if (playerId === otherPlayer.playerId) {
+        otherPlayer.destroy();
+      }
+    });
+  });
+  //키보드 인풋 받겠다
+  this.cursors = this.input.keyboard.createCursorKeys();
+  //'playerMoved'이벤트를 서버에서 수신하고 playerInfo에 수신받은 players[socket.id]정보 입력
+  this.socket.on('playerMoved', function (playerInfo) {
+    //otherPlayers 오브젝트
+  self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+    if (playerInfo.playerId === otherPlayer.playerId) {
+      otherPlayer.setRotation(playerInfo.rotation);
+      otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+    }
+  });
+});
+}
+
+function update() {
+
+  if (this.ship) {
+
+    if (this.cursors.left.isDown) {
+      this.ship.setAngularVelocity(-150);
+    } else if (this.cursors.right.isDown) {
+      this.ship.setAngularVelocity(150);
+    } else {
+      this.ship.setAngularVelocity(0);
+    }
+
+    if (this.cursors.up.isDown) {
+      this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
+    } else {
+      this.ship.setAcceleration(0);
+    }
+
+    this.physics.world.wrap(this.ship, 5);
+
+    var x = this.ship.x;
+    var y = this.ship.y;
+    var r = this.ship.rotation;
+
+    //배의 이전 움직임이 현재 값과 다르 'playerMovement'이벤트와 ,x,y,rotation값을 서버로 보냄
+    if (this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation)) {
+      this.socket.emit('playerMovement', {
+        x: this.ship.x,
+        y: this.ship.y,
+        rotation: this.ship.rotation
+      });
+    }
+
+    // save old position data
+    this.ship.oldPosition = {
+      x: this.ship.x,
+      y: this.ship.y,
+      rotation: this.ship.rotation
+    };
+  }
+
+}
+
+// add self.player
+function addPlayer(self, playerInfo) {
+  self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+  if (playerInfo.team === 'blue') {
+    self.ship.setTint(0x0000ff);
+  } else {
+    self.ship.setTint(0xff0000);
+  }
+  self.ship.setDrag(100);
+  self.ship.setAngularDrag(100);
+  self.ship.setMaxVelocity(200);
+}
+
+// add anoter players
+function addOtherPlayers(self, playerInfo) {
+  const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+  if (playerInfo.team === 'blue') {
+    otherPlayer.setTint(0x0000ff);
+  } else {
+    otherPlayer.setTint(0xff0000);
+  }
+  otherPlayer.playerId = playerInfo.playerId;
+  self.otherPlayers.add(otherPlayer);
+}
--- server.js
+++ server.js
@@ -1,13 +1,66 @@
 const express = require('express');
 var app = express();
 var server = require('http').Server(app)
-
-app.use('/assets',express.static('./public'))
-
+var io = require('socket.io')(server)
+var osc = require('osc.io')
+var players = {}
+osc(io)
+var oscServer = io.of('http://localhost/osc/servers/8000');
+var oscClient = io.of('http://localhost/osc/clients/8000');
+setInterval(function() {
+  oscClient.emit('message',['/osc/test',200])
+}, 1000)
+app.use('/assets', express.static('./public'))
+app.use('/scripts', express.static('./node_modules/phaser/dist/'))
+// app.use('/scripts', express.static('./node_modules/osc-js/lib'))
 app.get('/', function(req, res) {
-  res.sendFile(__dirname+'/index.html')
+  res.sendFile(__dirname + '/index.html')
 })
 
-server.listen(3000,function() {
-  console.log('Listening on ' +server.address().port);
+server.listen(3000, function() {
+  console.log('Listening on ' + server.address().port);
+
 })
+
+io.on('connection', function(socket) {
+  console.log('user connected : ', socket.id);
+  //player Setting
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 880) + 50,
+    y: Math.floor(Math.random() * 1820) + 50,
+    playerId: socket.id,
+    team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+  }
+  //send the players objec to the new player
+  socket.emit('currentPlayers', players);
+  //update all other players of the new player
+  socket.broadcast.emit('newPlayer', players[socket.id]);
+
+  socket.on('disconnect', function() {
+    //remove player from players object
+    delete players[socket.id]
+    //emit a message to all players to remove this player
+    io.emit('disconnect', socket.id)
+    console.log('user disconnected', socket.id);
+  })
+  // 'playerMovement'이벤트를 수신하고 movementData에 x,y,rotation값 수신
+  // 움직임 데이터를 해당 소켓 오브젝트에 반영함
+  // 그리고 이 플레이어 정보를 'playedMoved'라는 이벤트로 players[socekt.id]를 접속한 클라이언트 전체 송출
+  socket.on('playerMovement', function(movementData) {
+    players[socket.id].x = movementData.x;
+    players[socket.id].y = movementData.y;
+    players[socket.id].rotation = movementData.rotation;
+    // emit a message to all players about the player that moved
+    socket.broadcast.emit('playerMoved', players[socket.id]);
+
+  });
+})
+
+//random int 값 받기 , 나중에 캐릭터 설정 할 때 사용 가능할 듯
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// console.log(getRandomInt(3));
+// expected output: 0, 1 or 2
